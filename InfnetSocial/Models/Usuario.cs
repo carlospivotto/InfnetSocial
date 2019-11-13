61882149a1163c1b89a136320d237cf61c6030fd
Carlos Pivotto <carlospivotto@msn.com>
2019-11-13 11:47:41 -0300

    Usuario

--- InfnetSocial/Models/Usuario.cs
+++ InfnetSocial/Models/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace InfnetSocial.Models
 {
     public class Usuario
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
